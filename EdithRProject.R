eca045a4bccb4d87b9cd52611d051036cfdb6786
eplants <eplantsparis@patriots.uttyler.edu>
2015-11-30 21:21:35 -0600

    Final

--- EdithRProject.R
+++ EdithRProject.R
@@ -1,4 +1,4 @@
-## Edith Plants-Paris' Survey project in R ##
+## Edith Plants-Paris' Survey project in R  ##
 
 # Install and load dplyr and ggplot2
 install.packages("dplyr")
@@ -31,12 +31,12 @@ Weight <- merriami %>%
 
 # Option 1 Create a histogram looking at the distribution of weight
 # Also saves the figure as a pdf
-pdf("figures/scatterplot.pdf")
+pdf("figures/weightdistribution.pdf")
 ggplot(data = Weight, aes(x=weight)) + geom_histogram(aes(fill = ..count..),binwidth=1) + scale_fill_gradient("Count", low = "#ff0099", high = "#9900cc") + theme_bw() + ylab("Frequency") + xlab("Weight(g)") + scale_x_continuous(breaks=seq(0,100,5)) + scale_y_continuous(breaks=seq(0,800,100)) + ggtitle("Distribution of weight within D.merriami") + theme(plot.title = element_text(lineheight=.8, face="bold"))
 dev.off()
 
 # Option 2, in case option 1 does not work
-pdf("figures/scatterplot.pdf")
+pdf("figures/weightdistribution.pdf")
 qplot(weight, data = weight, ylab= "Frequency", xlab="Weight", binwidth = 1, geom="histogram")
 dev.off()
 
@@ -56,7 +56,7 @@ t.test(weight ~ sex, data=Sex_comparison)
 
 # Boxplot comparing weights of males and females
 # Also saves the figure as a pdf
-pdf("figures/scatterplot.pdf")
+pdf("figures/genderweights.pdf")
 ggplot(data = Sex_comparison, aes(x=species, y=weight , fill=sex)) + geom_boxplot() + scale_fill_manual(values=c("#ff0099", "#0099ff")) + theme_bw() + ylab("Weight (g)") + xlab("Species") + ggtitle("Comparison of weights between male and female D. merriami") + theme(plot.title = element_text(lineheight=.8, face="bold"))
 dev.off()
 
@@ -70,7 +70,7 @@ Avg <- aggregate(WeightYear$weight, list(year = WeightYear$year), mean)
 
 # Create a scatterplot to look at the mean weight per year and create a Add linear regression line that includes 95% confidence region
 # Also saves the figure as a pdf
-pdf("figures/scatterplot.pdf")
-ggplot(data=Avg, aes(x=year, y= x)) + geom_point(shape=8,size=5, colour="#9900cc") + geom_smooth(method=lm, colour="#9900cc") + ylab("Mean Weight (g)") + xlab("Year") + theme_bw()
+pdf("figures/weightovertime.pdf")
+ggplot(data=Avg, aes(x=year, y= x)) + geom_point(shape=16,size=5,color="#9900cc") + geom_smooth(method=lm, colour="#9900cc") + ylab("Mean Weight (g)") + xlab("Year") + theme_bw() + ggtitle("Mean Weight of D. merriami over Time") + scale_x_continuous(breaks=seq(1976,2003,2))
 dev.off()
 
